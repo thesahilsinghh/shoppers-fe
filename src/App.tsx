1adfbffe6a4bb59368fda1e350ddde83da6edd77
mohit <2003mohitkhemani@gmail.com>
2025-10-05 02:08:03 +0530

    shoppers-frontend setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='h-screen w-screen'>
+      <h1 className='text-5xl font-bold underline'>Working</h1>
+      <button className="btn btn-primary">Button</button>
+    </div>
+  )
+}
+
+export default App
